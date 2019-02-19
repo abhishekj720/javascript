fa2a5ab73156a2bde3ce672347b020364534e249
Abhishek Jha <abhjha3@publicisgroupe.net>
2019-02-19 13:15:12 +0530

    dsfs

---
+++ form1.js
@@ -0,0 +1,8 @@
+function show()
+{
+    var id1 = document.getElementById("username");
+    console.log(id1.value);
+    console.log(document.getElementById("pass").value);
+    console.log(document.getElementById("country").value);
+    console.log(document.getElementById("register").value);
+}
---
+++ prac1.js
@@ -0,0 +1,28 @@
+console.log("afhd")
+
+var x=10;
+
+if (typeof(x) == "number") {
+    console.log("x is a number");
+}
+    
+else
+    console.log("not a number");
+
+
+for(var i=1;i<11;i++)
+    {
+        console.log(i);
+    }
+    console.log(i);
+
+function clicka(){
+    alert("hi");
+    var testme = "xyz";
+}
+
+
+
+var buttons = document.getElementsByTagName('button');
+console.log(buttons);
+    
